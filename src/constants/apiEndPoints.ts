f54f15d32bd66476ab048f9d3ed1ce1839bcb120
jumaqulovyashnar <jumaqulovyashnar@gmail.com>
2026-04-25 15:14:21 +0500

    add

---
+++ src/constants/apiEndPoints.ts
@@ -0,0 +1,10 @@
+const API_ENDPOINTS = {
+  AUTH: {
+    LOGIN: "/auth/login",
+    FORGOT_PASSWORD: "/auth/forgot-password/",
+    VERIFY_PASSWORD: "/auth/verify-password/",
+  },
+  USER: {
+    USER_ME: "/user",
+  },
+};
