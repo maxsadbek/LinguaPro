67815d2545dd2fb5d3c3ed70312eca286a74e193
Mirjalol <nosirovmirjalol869@gmail.com>
2026-04-30 17:24:37 +0500

    add

--- src/hooks/teacher/groups/useRemoveStudentFromGroup.ts
+++ src/hooks/teacher/groups/useRemoveStudentFromGroup.ts
@@ -1,14 +1,17 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query'
-
 import { removeStudentFromGroup } from '@/api/service/teacher/group.service'
 
 export const useRemoveStudentFromGroup = (groupId: number) => {
   const queryClient = useQueryClient()
 
   return useMutation({
-    mutationFn: (studentId: number) => removeStudentFromGroup(groupId, studentId),
+    mutationFn: (studentId: number) =>
+      removeStudentFromGroup(groupId, studentId),
     onSuccess: async () => {
-      await queryClient.invalidateQueries({ queryKey: ['teacher', 'groups', 'my'] })
+      await queryClient.invalidateQueries({
+        queryKey: ['teacher', 'groups', 'my'],
+      })
+      await queryClient.invalidateQueries({ queryKey: ['students'] })
     },
   })
 }
